c9b9d00d966ccd8bc8fd8d5e8e11b0c8a463b0f3
Galang11404 <148953253+Galang11404@users.noreply.github.com>
2024-01-25 20:10:49 +0700

    Delete Pertemuan 10 directory

--- Pertemuan 10/Delete.py
+++
@@ -1,9 +0,0 @@
-from MATAKULIAH import *
-
-# Delete data
-A = Matakuliah()
-kodemk = "C1CX360T"
-
-A.deleteByKODEMK(kodemk)
-B = A.getAllData()
-print(B)
--- Pertemuan 10/show.py
+++
@@ -1,6 +0,0 @@
-from MATAKULIAH import *
-
-# Menampilkan semua data dari tabel mahasiswa
-A = Matakuliah()
-B = A.getAllData()
-print(B)
